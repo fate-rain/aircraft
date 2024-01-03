542e4cbb195cce9283eadf8c6fb774ac0898ad26
成雨 <1121352970@qq.com>
2024-01-03 12:55:02 +0800

    feat: update

---
+++ src/Experience.tsx
@@ -0,0 +1,10 @@
+function Experience() {
+    return (
+        <mesh>
+            <torusKnotGeometry/>
+            <meshNormalMaterial/>
+        </mesh>
+    )
+}
+
+export default Experience
--- src/main.tsx
+++ src/main.tsx
@@ -1,17 +1,16 @@
+import './index.css'
+
 import {createRoot} from 'react-dom/client'
 import {Canvas} from '@react-three/fiber'
 
-import './index.css'
+import Experience from "./Experience.tsx";
 
 const root = document.getElementById('root')!
 
 createRoot(root).render(
     <>
         <Canvas>
-            <mesh>
-                <torusKnotGeometry />
-                <meshNormalMaterial />
-            </mesh>
+            <Experience/>
         </Canvas>
     </>
 )
