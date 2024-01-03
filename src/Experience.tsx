365c9d3c4b1c694ed6b69d395b2dc5c173d3e6e3
成雨 <1121352970@qq.com>
2024-01-03 13:15:44 +0800

    feat: update

--- src/Experience.tsx
+++ src/Experience.tsx
@@ -1,9 +1,19 @@
 function Experience() {
     return (
-        <mesh>
-            <torusKnotGeometry/>
-            <meshNormalMaterial/>
-        </mesh>
+        <>
+            <mesh position-x={-2}>
+                <sphereGeometry/>
+                <meshBasicMaterial color="orange"/>
+            </mesh>
+            <mesh rotation-y={Math.PI * 0.25} position-x={2} scale={1.5}>
+                <boxGeometry/>
+                <meshBasicMaterial color='mediumpurple'/>
+            </mesh>
+            <mesh position-y={-1} rotation-x={-Math.PI * 0.5} scale={10}>
+                <planeGeometry/>
+                <meshBasicMaterial color='greenyellow'/>
+            </mesh>
+        </>
     )
 }
 
