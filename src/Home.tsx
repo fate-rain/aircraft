fda7d26501a8bef3a471c1896c49e62147037a72
成雨 <1121352970@qq.com>
2024-01-07 21:00:20 +0800

    feat: update

--- src/Home.tsx
+++ src/Home.tsx
@@ -6,12 +6,16 @@ import Part2 from "./components/Part2.tsx";
 function Home() {
 
   return (
-    <>
-        <Header/>
-        <CcDesign/>
-        <Part1/>
-        <Part2/>
-    </>
+      <>
+          <Header/>
+          <div className="flex justify-center">
+              <div className="max-w-[1440px]">
+                  <CcDesign/>
+                  <Part1/>
+                  <Part2/>
+              </div>
+          </div>
+      </>
   )
 }
 
