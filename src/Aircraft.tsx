815ede2da3c1efc399bb787e85c1a33e58c03301
成雨 <1121352970@qq.com>
2024-01-07 18:00:22 +0800

    feat: tailwindcss

--- src/Aircraft.tsx
+++ src/Aircraft.tsx
@@ -1,27 +1,38 @@
+import {MutableRefObject, useEffect, useRef} from "react";
+import * as THREE from 'three'
 import {TransformControls, useAnimations, useGLTF} from "@react-three/drei";
-import {useEffect} from "react";
+
+type Primitive = MutableRefObject<THREE.Object3D<THREE.Object3DEventMap>>
 
 /**
  * 飞机
  */
 function Aircraft() {
+    const aircraft = useRef()
     const model = useGLTF('./models/aircraft.glb')
 
     const animations = useAnimations(model.animations, model.scene)
 
     useEffect(() => {
-        const action = animations.actions['Take 001']
-        action?.fadeIn(0.5).play()
-
-        return () => {
-            action?.fadeOut(0.5)
-        }
+        // const action = animations.actions['Take 001']
+        // action?.fadeIn(0.5).play()
+        //
+        // return () => {
+        //     action?.fadeOut(0.5)
+        // }
     }, [animations])
 
     return (
-        <TransformControls>
-            <primitive object={model.scene} scale={3}/>
-        </TransformControls>
+        <>
+            <primitive
+                ref={aircraft}
+                object={model.scene}
+                scale={2.5}
+                position={[0, -0.7, 0]}
+                rotation={[Math.PI / 4, -Math.PI / 6, 0]}
+            />
+            <TransformControls object={aircraft as unknown as Primitive}/>
+        </>
     )
 }
 
--- src/Scene.tsx
+++ src/Scene.tsx
@@ -18,7 +18,7 @@ function Scene() {
                     fov: 45,
                     near: 0.1,
                     far: 200,
-                    position: [0, 3, 6]
+                    position: [0, 0, 6]
                 }}
             >
                 {/*环境*/}
