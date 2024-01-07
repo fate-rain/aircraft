6beafbabbf67c8f11d9cd01e148ef9572f75ac8c
成雨 <1121352970@qq.com>
2024-01-07 20:48:28 +0800

    feat: update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,6 +1,7 @@
 import CcDesign from "./components/CcDesign.tsx";
 import Header from "./components/Header.tsx";
 import Part1 from "./components/Part1.tsx";
+import Part2 from "./components/Part2.tsx";
 
 function Home() {
 
@@ -9,7 +10,7 @@ function Home() {
         <Header/>
         <CcDesign/>
         <Part1/>
-        <Part1/>
+        <Part2/>
     </>
   )
 }
