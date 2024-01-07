8abcaff9717070bb1a642c4bfbd5408cb06e5131
成雨 <1121352970@qq.com>
2024-01-07 19:04:54 +0800

    feat: update

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,6 @@
 import CcDesign from "./components/CcDesign.tsx";
 import Header from "./components/Header.tsx";
+import Part1 from "./components/Part1.tsx";
 
 function Home() {
 
@@ -7,6 +8,7 @@ function Home() {
     <>
         <Header/>
         <CcDesign/>
+        <Part1/>
     </>
   )
 }
---
+++ src/components/Part1.tsx
@@ -0,0 +1,11 @@
+import Cloud from "./Cloud.tsx";
+
+function Part1() {
+    return (
+        <div className="pt-[230px]">
+            <Cloud className="w-[806px] -translate-x-[117px]"/>
+        </div>
+    )
+}
+
+export default Part1
