a8bd47874a78695d01e1ccb02d070fc11d1a8fc4
成雨 <1121352970@qq.com>
2024-01-13 12:59:45 +0800

    feat(style): media

--- src/components/Title.tsx
+++ src/components/Title.tsx
@@ -4,7 +4,7 @@ import CcDesign from "./CcDesign.tsx";
 function Title() {
     return (
         <div className="flex justify-center mt-[153px] relative">
-            <CcDesign className="max-w-[350px] sm:max-w-[1441px]"/>
+            <CcDesign className="max-w-[360px] sm:max-w-[1441px]"/>
             <Cloud className="absolute -top-[25px] left-1/2 translate-x-[50px] sm:translate-x-[130px] w-[50px] sm:w-[140px]"/>
         </div>
     )
